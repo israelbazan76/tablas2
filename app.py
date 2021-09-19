b6f21dbc5316183f214fe7839fa460c19f83b32c
israelb <bazanni@globalhitss.com>
2021-09-19 00:53:36 -0300

    for mobile

--- app.py
+++ app.py
@@ -49,12 +49,12 @@ def show_tablas(num,start):
     form = TablasForm()
     if form.validate_on_submit():
         num = form.number.data
-    user_agent = parse(request.user_agent.string)
-    if user_agent.is_mobile:
+    #user_agent = parse(request.user_agent.string)
+    #if user_agent.is_mobile:
         cols = 1
         return render_template("mobile/show_tablas_v1.html", num=num,cols=cols,start=start,form=form)
-    else:
-        return render_template("show_tablas_v1.html", num=num,cols=cols,start=start,form=form)
+    #else:
+    #    return render_template("show_tablas_v1.html", num=num,cols=cols,start=start,form=form)
 
 
        
